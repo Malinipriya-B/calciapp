f9b6735969c8ddb59413d013cc78f097432fd063
Malinipriya-B <96307328+Malinipriya-B@users.noreply.github.com>
2022-03-30 09:04:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Calculator from "./components/cal";
+import "./App.css";
+
+
+function App() {
+  return(
+    <>
+    <Calculator />
+    </>
+  );
+}
+
+export default App;
